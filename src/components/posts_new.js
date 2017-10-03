07ccc8d41103dc4fec10096e8363441d709a3e99
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2017-10-03 16:05:02 +0300

    scaffold posts_new and fix routes

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mostafazahran on 10/3/17.
+ */
+import React, { Component } from 'react';
+
+export default class PostsNew extends Component {
+    render (){
+        return (
+            <div>
+                New Post!
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import PostsIndex from './components/posts_index';
 import Promise from 'redux-promise';
-
 import reducers from './reducers';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-          <div>
-              <Route path="/" component={PostsIndex}/>
-          </div>
-      </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/posts/new" component={PostsNew}/>
+                    <Route path="/" component={PostsIndex}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
